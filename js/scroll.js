0c32bb1171d86b740f6584b7f0bc46f1445ed568
Zinaida Razinkova <razinkova.zina@gmail.com>
2023-12-17 21:40:57 +0200

    scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', function () {
-    console.log(scrollY )
+    // console.log(scrollY )
     this.scrollY > 0 ? this.document.querySelector('.header').classList.add('scroll') : this.document.querySelector('.header').classList.remove('scroll')
 
 })
